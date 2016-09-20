f8ebce1b520f03751893c1f6e374927fa53075cb
Yavin <tech@alanzard.com>
2016-09-20 07:36:07 +0200

    Added OXfam to Partner block on About page

--- views/about.php
+++ views/about.php
@@ -7,10 +7,15 @@
             <p>Mining Pachena Zimbabwe enables you to map, find, browse, and search mining company hierarchies, community monitoring reports on mining impacts and mineral revenue payments to government. You can trace relationships between child mining companies and their parent companies for easy access to integrated annual reports produced by listed companies, where they’re registered to check for companies domiciled in tax havens which are prone to Illicit Financial Flows (IFFs) or find out who manages these companies to check for Politically Exposed Persons (PEPs) or persons with corrupt track record, community pictures on mining impacts on human rights and community development and how much a company is paying to government. You can also access the data referred to by clicking on the map.</p>
 <p>The information on this platform is publicly available, but it was previously hard to access and use for advocacy without exhausting resources. Tracking integrated annual reports of listed mining companies has been a painstaking exercise as some mining companies operating in Zimbabwe are privately registered. Yet detailed reports on financial performance, taxes paid to government, production, corporate social investments can be accessed through searching for the parent company which consolidates reports of its various mining companies. With improved accessibility to this data, you can more easily combat industry corruption and advocate for practice and policy changes that ensure that all citizens enjoy their fair share of mineral wealth.</p>
 <blockquote>“The absence of composite mapping of the mining and mineral revenue information that is currently publicly available is partly the cause for continued debate and, to some extent, confusion around mining information disclosure. Without an understanding of the mining information disclosure landscape, the calls for transparency and accountability by various actors have lacked traction because of wide generalisations. This has, in turn, made it difficult to give succinct and actionable recommendations on how to improve transparency in the mining sector.” <br /> - SERA May 2015 Report Mineral Revenue Disclosure and Understanding the Information Needs of Various Stakeholders</blockquote>
-<p>For more information please contact us on:</p>
-infor@zela.org
-Emmanuel@zela.org
-Mukasiri@zela.org
+<p>For more information please contact us on:
+<ul>
+    <li><a href="mailto:infor@zela.org">infor@zela.org</a></li>
+    <li><a href="mailto:Emmanuel@zela.org">Emmanuel@zela.org</a></li>
+    <li><a href="mailto:Mukasiri@zela.org">Mukasiri@zela.org</a></li>
+</ul>
+
+
+</p>
 
             <div class="matchbox-box">
               
@@ -32,8 +37,28 @@ Mukasiri@zela.org
               <p><a href="https://www.theengineroom.org/projects/institute-for-public-policy-research/">Learn more about how IPPR and The Engine Room developed this project.</a></p>
             </div>
 
-            <h2>Main Contributors</h2>
-
+            <h2>Partners</h2>
+            <div class="Block row">
+              <div class="col s4">
+                <a href="https://www.oxfam.org/" title="Oxfam Zimbabwe">
+                    <img src="/images/dist/oxfam.png" alt="Oxfam Zimbabwe" />
+                </a>
+              </div>
+              <div class="col s8">
+              <p class="Block-title"> <a href="https://www.oxfam.org/" title="Oxfam Zimbabwe">Oxfam Zimbabwe</a> </p>
+              <p>For nearly 20 years, Oxfam has worked to right one of the world’s biggest wrongs: Countries that are rich in natural resources are often poor and suffer from high rates of inequality, corruption, human rights abuse, and environmental degradation.</p>
+              <p>Oxfam’s work on extractive industries across the Oxfam confederation under a common set of goals and objectives. </p> 
+                <p>Our focus is on ensuring that:
+<ul><li>Governments and companies increase financial transparency and pro-poor accountability for extractive industries revenue generation and expenditure </li>
+<li>Governments and companies adopt and implement laws and policies respecting free, prior and informed consent (FPIC) </li>
+<li>Companies respect and governments protect civil society space and the human rights of communities impacted by extractive industries operations </li>
+<li>Gender justice becomes a central issue in global extractive industries reform efforts; and women’s rights are more progressively realized within the sector </li></ul>
+</p>
+<p>We work with partners in more than 30 countries – from across Africa, Asia, the Americas and Pacific – to achieve these goals.</p>
+              </div>
+            </div>
+        
+        <h2>Main Contributors</h2>
             <div class="Block row">
               <div class="col s4">
                 <a href="http://web.burza.hr/" title="web.burza, Digital Agency">
